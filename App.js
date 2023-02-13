3f349c912b41fb0b8b08caad070cdb2158fc9f23
dajkagabi <70899647+dajkagabi@users.noreply.github.com>
2023-02-13 23:27:07 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textstyle}>Hello React Native!</Text>
+
+      <Text style={styles.textstyle}>Let's go</Text>
+
+
+
+      <Button title='Click me!' onPress={() => alert
+        ("Hello I am React Native Button") /* disable = {true/false} */}>
+      </Button>
       <StatusBar style="auto" />
-    </View>
+    </View >
   );
 }
 
@@ -17,4 +27,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textstyle: {
+    fontSize: 20,
+    color: "blue",
+    fontWeight: "bold",
+    fontStyle: "italic",
+    // backgroundColor: "floralwhite",
+
+  }
 });
